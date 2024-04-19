15ff73eb44875f64bc3590ed651c431aeabaf008
Kartik Gulia <52398152+hotspur-75@users.noreply.github.com>
2024-04-19 14:21:22 +0530

    Add files via upload

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 import pygame
 from pygame.locals import *
+import numpy
 import random
 
 pygame.init()
  
 screen = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption("Bully-Sweeper")
+pygame.display.set_caption("batana bro")
  
 gameOn = True
 playing = False
@@ -18,40 +19,95 @@ p = pygame.Surface((30,30))
 p.fill((255,255,255))
 e = pygame.Surface((30,30))
 e.fill((0,0,0))
+fog = pygame.Surface((30,30))
+fog.fill((70,77,89))
 
-x = 55
-y = 55
+board_data = numpy.zeros((20,20))
 
-for i in range(10):
-    for j in range(10):
-        screen.blit(b,(x,y))
-        screen.blit(c,(x+2,y+2))
-        y+=50
-    x+=50
+def reckon_board(board_data):
+    for i in range(20):
+        for j in range(20):
+            board_data[i][j] = 0
+    count = 0
+    while count!= 20:
+        x = random.choice(range(20))
+        y = random.choice(range(20))
+        if board_data[x][y] == 0 and x + y != 0:
+            count+=1
+            board_data[x][y] = 9
+    for i in range(20):
+        for j in range(20):
+            if board_data[i][j]!=9:
+                mcount = 0
+                for x in [-1,0,1]:
+                    for y in [-1,0,1]:
+                        if i+x >= 0 and i+x < 20 and j+y >= 0 and j+y < 20 and board_data[i+x][j+y] == 9:
+                            mcount+=1
+                board_data[i][j] = mcount
+
+    for i in range(20):
+        for j in range(20):
+            board_data[i][j]+=10
+    
+    for [x,y] in [[0,0],[0,1],[0,2],[0,3],[1,0],[1,1],[1,2],[2,0],[2,1],[3,0]]:
+        board_data[x][y]-=10
+
+def reset_board(board_data):
+    for i in range(20):
+        for j in range(20):
+            board_data[i][j] = 0
+
+def print_board(vx,vy,board_data,mfont):
+    x = 55
     y = 55
+    for i in range(10):
+        for j in range(10):
+            screen.blit(b,(x,y))
+            screen.blit(c,(x+2,y+2))
+            text = mfont.render("%d" % (board_data[vx+i][vy+j]%10),True,(255,0,0))
+            if board_data[vx+i][vy+j] != 0 and board_data[vx+i][vy+j] < 10:
+                screen.blit(text,(x+20-text.get_width()/2,y+20-text.get_height()/2))
+            elif board_data[vx+i][vy+j] >= 10:
+                screen.blit(fog,(x+5,y+5))
+            y+=50
+        x+=50
+        y = 55
 
 clock = pygame.time.Clock()
 
 font = pygame.font.Font("BASKVILL.ttf", 48)
 fonter = pygame.font.Font("BASKVILL.ttf", 36)
+mfont = pygame.font.Font("BASKVILL.ttf",20)
 text = font.render("PLAY", True, (255, 0, 0))
 gtext = fonter.render("GIVE UP",True,(255,0,0))
 button_rect = pygame.Rect(770-text.get_width()/2, 300-text.get_height()/2, text.get_width(), text.get_height())
 buttoner_rect = pygame.Rect(770-gtext.get_width()/2, 400-gtext.get_height()/2, gtext.get_width(), gtext.get_height())
 
-px = random.choice(range(10))
-py = random.choice(range(10))
+px = 0
+py = 0
+vx = 0
+vy = 0
 
 t = 0
 
 screen.blit(text,(770-text.get_width()/2,300-text.get_height()/2)) 
+print_board(0,0,board_data=board_data,mfont=mfont)
 
 TIMEREVENT = pygame.USEREVENT + 1
 
 while gameOn:
 
     if playing:
-        screen.blit(p,(60+px*50,60+py*50)) 
+        screen.blit(p,(60+px*50,60+py*50))
+        surf = pygame.Surface((44,44))
+        surf.fill((219,140,61))
+        surf2 = pygame.Surface((40,40))
+        surf2.fill((0,0,0))
+        surf3 = pygame.Surface((20,20))
+        surf3.fill((219,140,61))
+        screen.blit(surf,(748,478))
+        screen.blit(surf2,(750,480))
+        screen.blit(surf3,(750+vx*2,480+vy*2))
     
     for event in pygame.event.get():
          
@@ -68,6 +124,23 @@ while gameOn:
             elif event.key == K_LEFT and playing and px != 0:
                 screen.blit(e,(60+px*50,60+py*50))
                 px-=1
+            elif event.key == K_w and playing and vy != 0 and py!=9:
+                vy-=1
+                py+=1
+            elif event.key == K_s and playing and vy != 10 and py!=0:
+                vy+=1
+                py-=1
+            elif event.key == K_a and playing and vx != 0 and px!=9:
+                vx-=1
+                px+=1
+            elif event.key == K_d and playing and vx != 10 and px!=0:
+                vx+=1
+                px-=1
+            for i in range(7):
+                for j in range(7):
+                    if abs(i-3)+abs(j-3) <= 3 and vx+px+i-3 >= 0 and vx+px+i-3 < 20 and vy+py+j-3 >= 0 and vy+py+j-3 < 20:
+                        board_data[vx+px+i-3][vy+py+j-3]%=10
+            print_board(vx,vy,board_data=board_data,mfont=mfont)
 
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if button_rect.collidepoint(event.pos):
@@ -75,21 +148,21 @@ while gameOn:
                 screen.fill((0,0,0),button_rect)
                 ttext = font.render("0:00",True,(255,0,0))
                 screen.blit(gtext,(770-gtext.get_width()/2,400-gtext.get_height()/2))
-                screen.blit(ttext,(770-ttext.get_width()/2,300-ttext.get_height()/2)) 
+                screen.blit(ttext,(770-ttext.get_width()/2,300-ttext.get_height()/2))
+                reckon_board(board_data=board_data) 
+                print_board(vx,vy,board_data=board_data,mfont=mfont)
                 pygame.time.set_timer(TIMEREVENT,1000)  
             elif buttoner_rect.collidepoint(event.pos) and playing:
                 t = 0
-                x = 55
-                y = 55
-                for i in range(10):
-                    for j in range(10):
-                        screen.blit(b,(x,y))
-                        screen.blit(c,(x+2,y+2))
-                        y+=50
-                    x+=50
-                    y = 55
+                reset_board(board_data=board_data)
+                vx = 0
+                vy = 0
+                px = 0
+                py = 0
+                print_board(vx,vy,board_data=board_data,mfont=mfont)
                 screen.fill((0,0,0),button_rect)
                 screen.fill((0,0,0),buttoner_rect)
+                screen.fill((0,0,0),(748,478,44,44))
                 playing = False
                 screen.blit(text,(770-text.get_width()/2,300-text.get_height()/2)) 
             
